43d585b6880751172e09b4a9daa1cb933cf47600
Infra BRMedia <infrabrmedia@Infras-MacBook-Pro.local>
2022-12-08 15:36:11 -0300

    add readme file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
-import './styles/main.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
  return (
-  <div className=''>
-
-  </div>
+  <Router>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/company">Empresa</Link></li>
+      <li><Link to="/services">Serviços</Link></li>
+      <li><Link to="/contact">Contato</Link></li>
+    </ul>
+  </Router>
  )
 }
 
