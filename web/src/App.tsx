cc127734c703ef073c7a56bd717691e086cab720
Infra BRMedia <infrabrmedia@Infras-MacBook-Pro.local>
2022-12-08 15:42:17 -0300

    add img/Logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
  return (
